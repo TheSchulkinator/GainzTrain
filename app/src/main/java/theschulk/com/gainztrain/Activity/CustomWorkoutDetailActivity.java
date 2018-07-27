01301f4aeee56f8c9e657ccfc59c8afbbf256bc9
Unknown <Gregschulkey@gmail.com>
2018-07-27 19:36:18 -0400

    Feature: Add custom workout detail activity

---
+++ app/src/main/java/theschulk/com/gainztrain/Activity/CustomWorkoutDetailActivity.java
@@ -0,0 +1,13 @@
+package theschulk.com.gainztrain.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomWorkoutDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_workout_detail);
+    }
+}
