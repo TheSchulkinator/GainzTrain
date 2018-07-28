b2138f4db0f225c2e9cf15a3c35f58e4f39c0f87
Unknown <Gregschulkey@gmail.com>
2018-07-28 16:39:25 -0400

    Feature: add menu to screen

--- app/src/main/java/theschulk/com/gainztrain/Activity/CustomWorkoutDetailActivity.java
+++ app/src/main/java/theschulk/com/gainztrain/Activity/CustomWorkoutDetailActivity.java
@@ -1,7 +1,13 @@
 package theschulk.com.gainztrain.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import theschulk.com.gainztrain.R;
 
 public class CustomWorkoutDetailActivity extends AppCompatActivity {
 
@@ -9,5 +15,38 @@ public class CustomWorkoutDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_workout_detail);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+
+        switch (item.getItemId()) {
+            case R.id.menu_body_tracker:
+                intent = new Intent(this, BodyTrackerActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.menu_custom_workouts:
+                intent = new Intent(this, CustomWorkoutActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.menu_home:
+                intent = new Intent(this, HomeActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.menu_history:
+                intent = new Intent(this, HistoryActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
