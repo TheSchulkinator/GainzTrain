325e6fe6fb0c06ef2cf70639b811347e8e9cbd48
Unknown <Gregschulkey@gmail.com>
2018-08-08 21:02:32 -0400

    Feature: added ability to edit weight on saved picture

--- app/src/main/java/theschulk/com/gainztrain/Adapters/ImageViewAdapter.java
+++ app/src/main/java/theschulk/com/gainztrain/Adapters/ImageViewAdapter.java
@@ -4,18 +4,23 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import theschulk.com.gainztrain.Activity.BodyTrackerActivity;
 import theschulk.com.gainztrain.Model.BodyTrackerModel;
@@ -48,12 +53,12 @@ public class ImageViewAdapter extends PagerAdapter {
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, final int position) {
-        View view = layoutInflater.inflate(R.layout.adapter_image_view, container, false);
+        final View view = layoutInflater.inflate(R.layout.adapter_image_view, container, false);
 
         TextView dateTextView = view.findViewById(R.id.picture_date_tv);
         TextView weightTextView = view.findViewById(R.id.current_weight_tv);
 
-        BodyTrackerModel bodyTrackerModel = GainzUtility.getBodyTrackerFromFile(files[position]);
+        final BodyTrackerModel bodyTrackerModel = GainzUtility.getBodyTrackerFromFile(files[position]);
 
         dateTextView.setText(bodyTrackerModel.getPictureDate());
         weightTextView.setText(bodyTrackerModel.getPictureWeight());
@@ -101,9 +106,55 @@ public class ImageViewAdapter extends PagerAdapter {
             }
         });
 
+        //allow the user to enter a new weight on long press
         weightTextView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
+                final AlertDialog.Builder builder;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    builder = new AlertDialog.Builder(context, android.R.style.Theme_Material_Light_Dialog);
+                } else {
+                    builder = new AlertDialog.Builder(context);
+                }
+
+                //create a layout inflater for the custom alert
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View dialogView = inflater.inflate(R.layout.edit_alert_dialog, null);
+
+                builder.setView(dialogView).
+                        setTitle(R.string.edit_user_weight_dialog).
+                        setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        EditText dialogWeightEdit = dialogView.findViewById(R.id.edit_alert_dialog_et);
+
+                        String newWeight = dialogWeightEdit.getText().toString();
+                        String timeStamp = bodyTrackerModel.getPictureDate();
+
+                        String imageFileName = "JPEG_" + timeStamp + "_" + newWeight + "_";
+                        File storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                        try {
+                            File image = File.createTempFile(
+                                    imageFileName,
+                                    ".jpg",
+                                    storageDir
+                            );
+                            files[position].renameTo(image);
+
+                            //Create an intent to reload screen when dialog closed
+                            Intent intent = new Intent(context, BodyTrackerActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                            context.startActivity(intent);
+                        }catch (IOException e){}
+                    }
+                })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // do nothing
+                            }
+                        })
+                        .show();
+
                 return false;
             }
         });
